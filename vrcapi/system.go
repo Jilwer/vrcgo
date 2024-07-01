baa082709b49122b612534000f5706df7dd4e6c9
Neckie <82233193+Neckie@users.noreply.github.com>
2024-07-01 16:04:44 +0200

    Added GetSystemTime function

--- vrcapi/system.go
+++ vrcapi/system.go
@@ -38,6 +38,36 @@ func (c *VRCApiClient) GetOnlineUsers() (string, error) {
 	return string(body), nil
 }
 
+// GetSystemTime returns the current time of the VRChat API.
+func (c *VRCApiClient) GetSystemTime() (string, error) {
+	u := c.BaseURL.String() + "/time"
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", c.UserAgent)
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New("API returned non-200 status code: " + resp.Status)
+	}
+
+	return string(body), nil
+}
+
 type SystemConfigResp struct {
 	VoiceEnableDegradation      bool   `json:"VoiceEnableDegradation"`
 	VoiceEnableReceiverLimiting bool   `json:"VoiceEnableReceiverLimiting"`
