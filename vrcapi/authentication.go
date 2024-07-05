e2b2b16afd8cb07f0aaacf56d4b142abc7fec155
Jilwer <165509268+Jilwer@users.noreply.github.com>
2024-07-05 17:26:05 -0400

    scaffold out functions for our authentication file

---
+++ vrcapi/authentication.go
@@ -0,0 +1,31 @@
+package vrcapi
+
+// TODO
+// CheckUserExists checks if a user exists in the VRChat API.
+func CheckUserExists(){
+
+}
+
+// TODO
+// GetCurrentUser returns the current user info in the VRChat API. Also user for logging in.
+func (c *VRCApiClient) GetCurrentUser(){
+
+}
+
+// TODO
+// Verifies the email OTP in the VRChat API and sets the TwoFactorAuthCookie.
+func (c *VRCApiClient) VerifyEmailOTP(){
+
+}
+
+// TODO
+// Verifies the TwoFactorAuthCookie in the VRChat API.
+func VerifyAuthToken(){
+
+}
+
+// TODO
+// Invalidates the login session in the VRChat API.
+func (c *VRCApiClient) Logout(){
+	
+}
