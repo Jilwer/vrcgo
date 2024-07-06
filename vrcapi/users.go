fb37c617e7828cf5a30864ba1c7132be18726884
Neckie <82233193+Neckie@users.noreply.github.com>
2024-07-06 13:13:45 +0200

    Added GetUserGroups function

--- vrcapi/users.go
+++ vrcapi/users.go
@@ -143,3 +143,63 @@ func (c *VRCApiClient) GetUserByID(userID string) (GetUserByIDResp, error) {
 
 	return user, nil
 }
+
+type GetUserGroupsResp []struct {
+	ID                string    `json:"id"`
+	Name              string    `json:"name"`
+	ShortCode         string    `json:"shortCode"`
+	Discriminator     string    `json:"discriminator"`
+	Description       string    `json:"description"`
+	IconID            string    `json:"iconId"`
+	IconURL           string    `json:"iconUrl"`
+	BannerID          string    `json:"bannerId"`
+	BannerURL         string    `json:"bannerUrl"`
+	Privacy           string    `json:"privacy"`
+	LastPostCreatedAt time.Time `json:"lastPostCreatedAt"`
+	OwnerID           string    `json:"ownerId"`
+	MemberCount       int       `json:"memberCount"`
+	GroupID           string    `json:"groupId"`
+	MemberVisibility  string    `json:"memberVisibility"`
+	IsRepresenting    bool      `json:"isRepresenting"`
+	MutualGroup       bool      `json:"mutualGroup"`
+	LastPostReadAt    time.Time `json:"lastPostReadAt"`
+}
+
+// GetUserGroups returns user information about a specific user using their ID.
+func (c *VRCApiClient) GetUserGroups(userID string) (GetUserGroupsResp, error) {
+	u := c.BaseURL.String() + "/users/" + userID + "/groups"
+
+	req, err := http.NewRequest("GET", u, nil)
+	if err != nil {
+		return GetUserGroupsResp{}, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("User-Agent", c.UserAgent)
+	req.AddCookie(&http.Cookie{Name: "auth", Value: c.AuthCookie})
+	req.AddCookie(&http.Cookie{Name: "twoFactorAuth", Value: c.TwoFactorAuthCookie})
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return GetUserGroupsResp{}, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return GetUserGroupsResp{}, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return GetUserGroupsResp{}, errors.New("API returned non-200 status code: " + resp.Status)
+	}
+
+	var groups GetUserGroupsResp
+	err = json.Unmarshal(body, &groups)
+	if err != nil {
+		return GetUserGroupsResp{}, err
+	}
+
+	return groups, nil
+}
